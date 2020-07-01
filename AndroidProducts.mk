6f2ff80ed1c124cda1e9a9350aba19cdd11c64b9
together08 <together08@yeah.net>
2020-07-01 13:59:27 +0800

    exthm-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_nx563j.mk
+    $(LOCAL_DIR)/exthm_nx563j.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_nx563j-user \
-    lineage_nx563j-userdebug \
-    lineage_nx563j-eng
+    exthm_nx563j-user \
+    exthm_nx563j-userdebug \
+    exthm_nx563j-eng
--- exthm_nx563j.mk
+++ exthm_nx563j.mk
@@ -19,7 +19,7 @@ $(call inherit-product, device/nubia/nx563j/full_nx563j.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_nx563j
+PRODUCT_NAME := exthm_nx563j
 PRODUCT_DEVICE := nx563j
 PRODUCT_MANUFACTURER := nubia
 PRODUCT_MODEL := NX563J
